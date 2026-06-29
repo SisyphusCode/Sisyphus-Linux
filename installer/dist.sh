8563ef43d4f21686b290186628ff0d8ae50434c6
Kenny Glowner <SisyphusCode@protonmail.com>
2026-06-29 12:34:58 -0500

    Fix dist.sh to include modules directory in tarball for COPR build

--- installer/dist.sh
+++ installer/dist.sh
@@ -5,5 +5,5 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 VERSION="${1:-1.0.0}"
 OUT="${2:-${ROOT}/installer/sisyphus-installer-config-${VERSION}.tar.gz}"
 
-tar czf "${OUT}" -C "${ROOT}" installer/settings.conf installer/branding
+tar czf "${OUT}" -C "${ROOT}" installer/settings.conf installer/branding installer/modules
 echo "Wrote ${OUT}"
